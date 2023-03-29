6e251643ce3458be47af4a1b37b70e277349bbbc
shalwingeorge771 <124098326+shalwingeorge771@users.noreply.github.com>
2023-03-29 17:38:52 -0400

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -1,15 +1,15 @@
 def encode(password):
-    password_string = str(password) # converts integer password into a string
-    pw_digits = [] # empty array to put individual digits of password into
+    password_string = str(password) # integer password to a string
+    pw_digits = [] # empty array
     encoded_password = ""
 
-    for i in range(len(password_string)): # adds digits from password into array
+    for i in range(len(password_string)): # password into array
         pw_digits.append(password_string[i])
 
     for i, digit in enumerate(pw_digits): # converts each element back into a string
         pw_digits[i] = int(digit)
 
-    for digit in pw_digits: # encodes elements and inserts them into string variable
+    for digit in pw_digits: # inserts elements into string variable
         if digit in range(0, 7):
             digit += 3
             encoded_password += str(digit)
@@ -23,39 +23,4 @@ def encode(password):
             encoded_password += "3"
 
     return encoded_password
-
-def decode(secret):
-    ta_da = ''
-    for i in secret:
-        receiver = int(i)
-        receiver -= 3
-        if receiver < 0:
-            receiver += 10
-        ta_da += str(receiver)
-    print(f"The encoded password is {secret}, and the original password is {ta_da}")
-    print("")
-
-
-if __name__ == "__main__": # main function
-    stop_menu = False
-    user_password = 0
-    user_encoded_password = ""
-
-    while stop_menu is False: # menu loop
-        print("Menu")
-        print("-------------")
-        print("1. Encode \n2. Decode \n3. Quit\n")
-        choice_user = int(input("Please enter an option: "))
-
-        if choice_user == 1: # encodes user password
-            user_password = str(input("Please enter your password to encode: "))
-            user_encoded_password = encode(user_password)
-            print("Your password has been encoded and stored!")
-
-        elif choice_user == 2:
-            decode(user_encoded_password)
-        elif choice_user == 3: # breaks loop and ends program
-            stop_menu = True
-            break
-        print()
 
